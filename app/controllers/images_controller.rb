eb0b3a0fb94846ffe7b8399d23f294e83be24877
Mike Adeleke <madeleke13@gmail.com>
2014-03-07 13:10:34 -0600

    updates

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,25 @@
 class ImagesController < ApplicationController
-  params[:files].each do |file|
-    i = Image.new(property_id: @property.id)
-    i.photo = file
-    i.photo = File.open('somewhere')
-    i.save!
+
+  before_action :get_property
+  
+  private
+  
+  def get_property
+    @property = Property.where(id: params[:property_id]).first
   end
+  
+  public
+
+  def create
+    params[:files].each do |file|
+      i = Image.new(property_id: @property.id)
+      i.photo = file
+      i.photo = File.open('somewhere')
+      i.save!
+    end
+    
+    flash[:notice] = "Successfully uploaded Images"
+    redirect_to properties_path
+  end
+
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ActiveRecord::Base
   belongs_to :property
   belongs_to :room
+  mount_uploader :photo, PhotoUploader
 end
