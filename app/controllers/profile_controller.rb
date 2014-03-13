218195c30b6a8c7cd02ae91327566b939b00e7ae
Mike Adeleke <madeleke13@gmail.com>
2014-03-13 00:38:13 -0500

    added profile form

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -11,18 +11,24 @@ class ProfileController < ApplicationController
   end
 
   def new
-    @user = User.find(params[:user_id])
-    @profile = @user.profile.new
+    @profile = Profile.new
+    @profile.user = current_user
   end
 
   def create
-    @user = User.find(params[:user_id])
-    @profile = @user.profile.new(profile_params)
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    
+    if @profile.save
+      redirect_to homes_index_path
+    else
+      render :new
+    end
   end
 
   private 
 
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :year, :major, :activities_and_clubs, :looking_for_place, :open_to_roommates, :price_range, :user_id)
+    params.require(:profile).permit(:first_name, :last_name, :year, :major, :activities_and_clubs, :looking_for_place, :open_to_roommates, :price_range)
   end
 end
