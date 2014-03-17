c9fdd1682aac83c947a95c4bab2c5bbf44e6f32d
Mike Adeleke <madeleke13@gmail.com>
2014-03-16 22:05:47 -0500

    valdiations added to models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,10 @@
 class Listing < ActiveRecord::Base
+  validates :schedule, presence: true
+  validates :open_to_student_only, presence: true
+  validates :rent_per_month, presence: true
+  validates :rent_per_month, numericality: true
+  validates :is_available, presence: true
+
   belongs_to :user
   belongs_to :property
   has_many :appointments
