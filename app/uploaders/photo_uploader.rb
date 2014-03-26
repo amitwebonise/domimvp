9a80ff75c6c18aa81addb0a4be52ac3f538157f7
Mike Adeleke <madeleke13@gmail.com>
2014-03-26 01:25:24 -0400

    added styling to property

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,11 +1,15 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process :resize_to_fit => [300, 225]
+  process :resize_to_fit => [500, 350]
 
   version :thumb do
     process :resize_to_fill => [300,225]
   end
+
+  version :gallery do
+    process :resize_to_fill => [500,350]
+  end  
 
   # version :thumb do
   #   process :resize_to_limit => [200, 200]
